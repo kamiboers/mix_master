41057eed879c3397217326293544fa974496b57a
Kami Boers <kamiboers@gmail.com>
2016-04-11 00:07:52 -0600

    styling changes

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,4 @@
 class Playlist < ActiveRecord::Base
-	has_many :playlist_songs
+	has_many :playlist_songs, :dependent => :delete_all
   has_many :songs, through: :playlist_songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  has_many :playlist_songs, :dependent => :delete_all
 
   validates :title, presence: true
 end
